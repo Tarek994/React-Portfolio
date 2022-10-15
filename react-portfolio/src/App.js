3bc91b75dcc2f50439819d5a07f65cfe10d9dbfb
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-15 21:45:45 +0200

    chore(app-root)

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -8,6 +8,7 @@ const App = () =>  {
     < >
     <Router>
       <Routes>
+        
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/experience" element={<Experience/>}/>
