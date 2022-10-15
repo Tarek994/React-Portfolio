fa3226c07e2f33dc32109b036a3d623932f5f96f
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-16 01:36:50 +0200

    add navbar links

--- react-portfolio/src/components/NavBar/NavBar.js
+++ react-portfolio/src/components/NavBar/NavBar.js
@@ -1,8 +1,19 @@
 import "./NavBar.css"
-
+import { Link } from "react-router-dom"
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navbar">
+       <div className="toggle-button"> 
+         <button> </button>
+       </div>
+       <div className="links">
+        
+        <Link to="/"> Home</Link>
+        <Link to="/projects"> Projects</Link>
+        <Link to="/experience"> Experience</Link>
+
+       </div>
+    </div>
   )
 }
 
