fd516005ee5b414200e3484cf8e427d192c6f5b9
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-17 23:59:52 +0200

    add image to project

--- react-portfolio/src/Pages/Projects/Projects.js
+++ react-portfolio/src/Pages/Projects/Projects.js
@@ -1,13 +1,13 @@
 import "./Projects.css"
 import { ProjectItem } from "../../components/index";
-
+import pro_01 from "../../assets/images/pro_01"
 
 const Projects = () => {
   return (
     <div className="projects">
       <h1>My personal Projects</h1>
       <div className="projectsList">
-      <ProjectItem name="Social Media Website" />
+      <ProjectItem name="Social Media Website" image={pro_01} />
       <ProjectItem/>
 
       
