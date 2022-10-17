c7b7320fe7c0232c68bd03102457087c0a608547
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-17 21:23:09 +0200

    add first time line element

--- react-portfolio/src/Pages/Experience/Experience.js
+++ react-portfolio/src/Pages/Experience/Experience.js
@@ -3,11 +3,20 @@ import { VerticalTimeline,
         VerticalTimelineElement
 } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
-
+import SchoolIcon from "@mui/icons-material/School"
 
 const Experience = () => {
   return (
-    <div>here is my experience</div>
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement className="vertical-timeline-element--education"
+        date="2010 - 2014"
+        iconStyle={{background:"#3e497a",color:"#fff"}}
+        icon={<SchoolIcon/>}>
+        <h3 className="vertical-timeline-element-title">My Random High School, Random Place, Random State </h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   )
 }
 
