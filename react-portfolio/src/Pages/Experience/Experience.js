ba696c932989c5dbd19282e998715d20356b26f3
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-17 21:03:58 +0200

    import vertical-time-line

--- react-portfolio/src/Pages/Experience/Experience.js
+++ react-portfolio/src/Pages/Experience/Experience.js
@@ -1,5 +1,8 @@
 import "./Experience.css"
-
+import { VerticalTimeline,
+        VerticalTimelineElement
+} from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
 
 
 const Experience = () => {
