c8d6569456b4703afc1c0fd61514cbd0cfb163ac
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-18 01:26:50 +0200

    chore(app-root)

--- react-portfolio/src/Pages/Projects/Projects.js
+++ react-portfolio/src/Pages/Projects/Projects.js
@@ -1,5 +1,6 @@
 import "./Projects.css"
 import { ProjectItem } from "../../components/index";
+import proimg from "../../assets/images/pro1";
 
 
 const Projects = () => {
@@ -7,7 +8,7 @@ const Projects = () => {
     <div className="projects">
       <h1>My personal Projects</h1>
       <div className="projectsList">
-      <ProjectItem name="Social Media Website"  />
+      <ProjectItem name="Social Media Website"  image={proimg}/>
       <ProjectItem/>
 
       
