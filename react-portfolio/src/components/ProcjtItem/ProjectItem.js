77666a98712199e22e5e7bf107c55c8be32ba3dc
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-18 00:54:53 +0200

    chore(app-root)

--- react-portfolio/src/components/ProcjtItem/ProjectItem.js
+++ react-portfolio/src/components/ProcjtItem/ProjectItem.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import "./ProjectItem.css"
+
 
 const ProjectItem = ({image, name}) => {
   return (
     <div className="ProjectItem">
-        <div className='bgImage' style={{backgroundImage: `url(${image})`}}/>
+        <div className='bgImage' />
         <h1>{name} </h1>
     </div>
   )
 }
 
 export default ProjectItem
+
+// style={{backgroundImage: `url(${image})`}}
