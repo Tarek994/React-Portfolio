7c192ed3a890c44a6ac3f36b841f9a741e9783bd
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-19 00:59:32 +0200

    import useParams and use it

--- react-portfolio/src/Pages/ProjectDisplay/ProjectDisplay.js
+++ react-portfolio/src/Pages/ProjectDisplay/ProjectDisplay.js
@@ -1,6 +1,10 @@
 import "./ProjectDisplay.css"
+import { useParams } from "react-router-dom"
+
 
 const ProjectDisplay = () => {
+
+    const {id} = useParams();
   return (
     <div>ProjectDisplay</div>
   )
