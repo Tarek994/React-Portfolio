70861414706cc528ad83a0b0bf1955daa56a67ac
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-19 00:55:49 +0200

    add ProjectDisplay to App Rout

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Home, Projects, Experience} from "./Pages/index"
 import { NavBar} from "./components/index";
 import {Footer} from "./Sections/index"
+import ProjectDisplay from "./Pages/ProjectDisplay/ProjectDisplay";
 
 const App = () =>  {
   return (
@@ -12,6 +13,8 @@ const App = () =>  {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
+        <Route path="/project/;id" element={<ProjectDisplay/>}/>
+
         <Route path="/experience" element={<Experience/>}/>
 
       </Routes>
